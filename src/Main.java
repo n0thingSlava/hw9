ea110315818762abf6a812194112ff06868d0671
Slav1k <maznevslava@yandex.ru>
2022-08-24 15:43:38 +0500

    dz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        String[] nameAouthor = {"S. King", "Book Publishing Ltd."};
+        int[] age = {100};
+        for (int i = 0; i < nameAouthor.length; i++) {
+            System.out.println(nameAouthor[i]);
+        }
+        for (int i = 0; i < age.length; i++) {
+            System.out.println(age[i]);
 
-        System.out.println("Hello world!");
+        }
+
+        }
     }
-}
